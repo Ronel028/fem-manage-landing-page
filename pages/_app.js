39d05ca82ad17fadb68847e46d1b6ed8a609be68
Ronel028 <floridaronel15@gmail.com>
2023-04-13 07:08:51 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import { Be_Vietnam_Pro } from "next/font/google";
+import "@/styles/globals.scss";
+
+const beVietnamPro = Be_Vietnam_Pro({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={beVietnamPro.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
